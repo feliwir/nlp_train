f2874a59157a48914b0088d91d77fc10f803e053
Stephan Vedder <stephan.vedder@gmail.com>
2022-05-25 21:25:12 +0200

    Init commit

---
+++ train.py
@@ -0,0 +1,97 @@
+import collections
+import pathlib
+
+import tensorflow as tf
+
+from tensorflow.keras import layers
+from tensorflow.keras import losses
+from tensorflow.keras import utils
+from tensorflow.keras.layers import TextVectorization
+
+import tensorflow_text as tf_text
+
+batch_size = 32
+seed = 42
+
+train_dir = "./data/de/"
+
+raw_train_ds = utils.text_dataset_from_directory(
+    train_dir,
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='training',
+    seed=seed)
+
+# Create a validation set.
+raw_val_ds = utils.text_dataset_from_directory(
+    train_dir,
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='validation',
+    seed=seed)
+
+VOCAB_SIZE = 10000
+
+binary_vectorize_layer = TextVectorization(
+    max_tokens=VOCAB_SIZE,
+    output_mode='binary')
+
+MAX_SEQUENCE_LENGTH = 250
+
+int_vectorize_layer = TextVectorization(
+    max_tokens=VOCAB_SIZE,
+    output_mode='int',
+    output_sequence_length=MAX_SEQUENCE_LENGTH)
+
+# Make a text-only dataset (without labels), then call `TextVectorization.adapt`.
+train_text = raw_train_ds.map(lambda text, labels: text)
+binary_vectorize_layer.adapt(train_text)
+int_vectorize_layer.adapt(train_text)
+
+def binary_vectorize_text(text, label):
+  text = tf.expand_dims(text, -1)
+  return binary_vectorize_layer(text), label
+
+def int_vectorize_text(text, label):
+  text = tf.expand_dims(text, -1)
+  return int_vectorize_layer(text), label
+
+# Retrieve a batch (of 32 reviews and labels) from the dataset.
+text_batch, label_batch = next(iter(raw_train_ds))
+first_question, first_label = text_batch[0], label_batch[0]
+
+int_train_ds = raw_train_ds.map(int_vectorize_text)
+int_val_ds = raw_val_ds.map(int_vectorize_text)
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+def configure_dataset(dataset):
+  return dataset.cache().prefetch(buffer_size=AUTOTUNE)
+
+int_train_ds = configure_dataset(int_train_ds)
+int_val_ds = configure_dataset(int_val_ds)
+
+
+def create_model(vocab_size, num_labels):
+  model = tf.keras.Sequential([
+      layers.Embedding(vocab_size, 64, mask_zero=True),
+      layers.Conv1D(64, 5, padding="valid", activation="relu", strides=2),
+      layers.GlobalMaxPooling1D(),
+      layers.Dense(num_labels)
+  ])
+  return model
+
+# `vocab_size` is `VOCAB_SIZE + 1` since `0` is used additionally for padding.
+int_model = create_model(vocab_size=VOCAB_SIZE + 1, num_labels=3)
+int_model.compile(
+    loss=losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer='adam',
+    metrics=['accuracy'])
+history = int_model.fit(int_train_ds, validation_data=int_val_ds, epochs=5)
+
+
+# print("Linear model on binary vectorized data:")
+# print(binary_model.summary())
+
+print("ConvNet model on int vectorized data:")
+print(int_model.summary())
